d50ff27ee7ae76f86d42ea590cc2ec3d85273fa8
Moulun <Dylan.moulun59@outlook.fr>
2023-06-21 22:26:07 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<section class="btn">
+<ul>
+        <a href="?jours=Lundi">Lundi</a>
+        <a href="?jours=Mardi">Mardi</a>
+        <a href="?jours=Mercredi">Mercredi</a>
+        <a href="?jours=Jeudi">Jeudi</a>
+        <a href="?jours=Vendredi">Vendredi</a>
+        <a href="?jours=Samedi">Samedi</a>
+        <a href="?jours=Dimanche">Dimanche</a>
+        </ul>
+    
+</section>
+
+<div class="img1">
+<?php
+echo $_GET['lundi']
+
+
+
+
+
+?>
+
+</div>
+
+
+
+</body>
+</html>
